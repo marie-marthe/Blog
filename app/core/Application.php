4a9f726a2275fc4479f5b28f7261a2fdc1f54317
TELBOIS Marie-Marthe <marie-marthe@macbook-air-de-telbois.home>
2019-07-24 21:15:03 -0400

    first commit

--- app/core/Application.php
+++ app/core/Application.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace core;
+namespace app\core;
 
 use http\Env\Response;
 use Psr\Http\Message\ResponseInterface;
@@ -10,18 +10,44 @@ use Psr\Http\Message\ServerRequestInterface;
 class Application
 {
 
-public function run(ServerRequestInterface $request): ResponseInterface {
+    private $modules = [];
 
-$uri = $request->getUri()->getPath();
-if (!empty($uri) && _$uri[-1] === "/") {
-return(new Response())
-->withStatus(301)
-->withHeader('Location', substr($uri, 0, -1));
-}
-$response = new Response();
-$response->getBody()->write('Bonjour');
-return $response;
 
-}
+    /**
+    * Application constructor.
+    * @param string[] $modules Liste des modules à charger
+
+     */
+
+
+    public function __construct(array $modules = [] )
+    {
+        foreach ($modules as $module) {
+            $this->modules[] = new $module();
+        }
+
+    }
+
+    public function run(ServerRequestInterface $request): ResponseInterface
+    {
+        {
+            $uri = $request->getUri()->getPath();
+            if (!empty($uri) && _$uri[-1] === "/") {
+            return (new Response())
+                ->withStatus(301)
+                ->withHeader('Location', substr($uri, 0, -1));
+        }
+    if ($uri === '/blog/mon-article')
+
+    {
+        return new \GuzzleHttp\Psr7\Response(200, [], '<h1>BIENVENUE SUR LE BLOG</h1>');
+
+    }
+
+
+        }
+
+return new \GuzzleHttp\Psr7\Response(404, [], '<h1>ERREUR 404</h1>');
+
 
 }
